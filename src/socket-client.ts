56965defcfe3c19d25cfdb8b00ac9edd48336aee
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-11-19 18:07:18 -0600

    Cliente - Detectar conexión y desconexión

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,10 +1,27 @@
-import { Manager } from 'socket.io-client';
+import { Manager, Socket } from 'socket.io-client';
 
 export const connectToServer = () => {
 
     const manager = new Manager('http://localhost:3000/socket.io/socket.io.js');
     
     const socket = manager.socket('/');
-    console.log({ socket });
 
+    addListeners( socket );
+
+}
+
+
+const addListeners = ( socket: Socket ) => {
+    const serverStatusLabel = document.querySelector('#server-status');
+
+    // Escuchar el estado de la conexion
+    socket.on('connect', () => {
+        serverStatusLabel.innerHTML = 'connected';
+    })
+    
+    socket.on('disconnect', () => {
+        serverStatusLabel.innerHTML = 'disconnected';
+    })
+    // Hablar
+    // socket.emit
 }
